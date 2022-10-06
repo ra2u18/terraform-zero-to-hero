08057c3c0b48a3a76899e39e313de0ca8705c055
netlemon <netlemon2021@gmail.com>
2022-10-06 16:35:17 +0300

    changed source of prod webserver_cluster

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  source =  "github.com/ra2u18/tf-modules-test//services/webserver-cluster?ref=v0.0.1"
 
   cluster_name           = "webservers-prod"
   db_remote_state_bucket = "tf-state-bucket-rick"
